e25b8162e23d6716b5adf370cb59d1a6cf6fffc2
neutcomp <bjornvdneut+github@gmail.com>
2020-04-18 07:00:54 +0200

    Added Cypress testing scripts

---
+++ cypress/integration/examples/index.spec.js
@@ -0,0 +1,23 @@
+/// <reference types="cypress" />
+
+context('Window', () => {
+  beforeEach(() => {
+    cy.visit('/')
+  })
+
+  it('Check all elements extists', () => {
+    cy.get('h1').contains('Welkom bij spelling oefenen');
+    cy.get('[data-cy=je-naam]').contains('Je naam');
+    cy.get('[data-cy=categorie]').contains('Categorie');
+    cy.get('[data-cy=aantal-vragen]').contains('Aantal te oefenen');
+    cy.get('h2').contains('Statistieken');
+  });
+
+  it('Check validation', () => {
+    cy.get('[data-cy=start]').click();
+    cy.get('[data-cy=validation-message]').contains('Je naam is niet ingevuld');
+    cy.get('[data-cy=voornaam').type('Bjorn');
+    cy.get('[data-cy=start]').click();
+    cy.get('[data-cy=validation-message]').contains('Je hebt geen categorie geselecteerd');
+  });
+});
